846983cd46d49f4044ba5c264bae2712a2606eb6
ShanmukhaKommuri <srinivaskommuri9204@gmai.com>
2025-10-18 14:48:34 +0530

    database

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-export default function Home() {
+import { db } from '@/lib/db'
+export default async function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Button>
