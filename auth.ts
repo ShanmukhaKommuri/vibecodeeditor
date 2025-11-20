77944a0186811d71876f43074401f0ab5d2820b6
ShanmukhaKommuri <srinivaskommuri9204@gmai.com>
2025-11-20 22:24:18 +0530

    Authentication

---
+++ auth.ts
@@ -0,0 +1,107 @@
+import NextAuth from "next-auth"
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import authConfig from "./auth.config"
+import { db } from "@/lib/db"
+import { getAccountByUserId, getUserById } from "./modules/auth/actions"
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    callbacks: {
+        async signIn({ user, account, profile }) {
+            if (!user || !account) return false;
+
+            const existingUser = await db.user.findUnique({
+                where: {
+                    email: user.email!
+                }
+            })
+
+            // if we don't found a user then we create a new user
+
+            if (!existingUser) {
+                const newUser = await db.user.create({
+                    data: {
+                        email: user.email!,
+                        name: user.name,
+                        image: user.image,
+
+                        accounts: {
+                            // @ts-ignore
+                            create: {
+                                type: account.type,
+                                provider: account.provider,
+                                providerAccountId: account.providerAccountId,
+                                refresh_token: account.refresh_token,
+                                access_token: account.access_token,
+                                expires_at: account.expires_at,
+                                token_type: account.token_type,
+                                scope: account.scope,
+                                id_token: account.id_token,
+                                session_state: account.session_state,
+                            },
+                        },
+                    }
+                });
+
+                if (!newUser) return false;
+            }
+            else {
+                const existingAccount = await db.account.findUnique({
+                    where: {
+                        provider_providerAccountId: {
+                            provider: account.provider,
+                            providerAccountId: account.providerAccountId
+                        }
+                    }
+                });
+
+                if (!existingAccount) {
+                    await db.account.create({
+                        data: {
+                            userId: existingUser.id,
+                            type: account.type,
+                            provider: account.provider,
+                            providerAccountId: account.providerAccountId,
+                            refresh_token: account.refresh_token,
+                            access_token: account.access_token,
+                            expires_at: account.expires_at,
+                            token_type: account.token_type,
+                            scope: account.scope,
+                            id_token: account.id_token,
+                            // @ts-ignore
+                            session_state: account.session_state,
+                        }
+                    });
+                }
+            }
+            return true;
+
+        },
+        async jwt({ token, user, account }) {
+            if (!token.sub) return token;
+
+            const existingUser = await getUserById(token.sub);
+
+            if (!existingUser) return token;
+
+            const existingAccount = await getAccountByUserId(existingUser.id);
+
+            token.name = existingUser.name;
+            token.email = existingUser.email;
+            token.role = existingUser.role;
+
+            return token;
+        },
+        // To control what data is sent to the client in the session object.
+        // It runs every time a session is created or accessed for example, when you call getSession() or use the useSession() hook on the client side.
+        async session({ session, token }) {
+            if (token.sub && session.user) {
+                session.user.id = token.sub;
+                session.user.role = token.role;
+            }
+            return session;
+        }
+    },
+    secret: process.env.AUTH_SECRET,
+    adapter: PrismaAdapter(db as any),
+    session: { strategy: "jwt" },
+    ...authConfig
+})
